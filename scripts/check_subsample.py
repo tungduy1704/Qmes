e8890bbf720cc3b692f29a7deaa5161454a0c816
Dao Duy Tung <daoduytung1704@gmail.com>
2026-06-15 09:06:29 +0700

    Remove check/test scripts from tracking, add to gitignore

--- scripts/check_subsample.py
+++
@@ -1,9 +0,0 @@
-from sklearn.utils import resample
-import numpy as np
-from Qmes.data.classification import load_classification_datasets
-
-# Chạy 2 lần, in shape và 5 rows đầu của CDC Diabetes
-datasets = load_classification_datasets()
-X, y = datasets["CDC Diabetes"]
-print(X.shape)
-print(X[:3])
