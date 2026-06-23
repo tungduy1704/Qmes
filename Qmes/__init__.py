af18c293ad520dbe1aa1650791133e438a4d9394
Dao Duy Tung <daoduytung1704@gmail.com>
2026-06-23 11:20:05 +0700

    Export run_loo_evaluation and filter_degenerate_datasets in public API

--- Qmes/__init__.py
+++ Qmes/__init__.py
@@ -2,8 +2,8 @@
 __version__ = "0.1.0"
 
 from Qmes.extractors import get_extractor, ClassificationExtractor, RegressionExtractor
-from Qmes.evaluators import get_evaluator, ClassificationEvaluator, RegressionEvaluator
-from Qmes.recommender import get_recommender, load_default_recommender, PairwiseRecommender
+from Qmes.evaluators import get_evaluator, ClassificationEvaluator, RegressionEvaluator, filter_degenerate_datasets
+from Qmes.recommender import get_recommender, load_default_recommender, PairwiseRecommender, run_loo_evaluation
 from Qmes.inference import recommend, evaluate_recommendation, preprocess_new_dataset
 from Qmes.circuits import CIRCUIT_POOL, get_circuit_names
 
@@ -11,6 +11,7 @@ __all__ = [
     "__version__",
     "get_extractor", "ClassificationExtractor", "RegressionExtractor",
     "get_evaluator", "ClassificationEvaluator", "RegressionEvaluator",
+    "run_loo_evaluation", "filter_degenerate_datasets",
     "get_recommender", "load_default_recommender", "PairwiseRecommender",
     "recommend", "evaluate_recommendation", "preprocess_new_dataset",
     "CIRCUIT_POOL", "get_circuit_names",
